4492fea7adfe222bd46ab7785eb6700394548490
LucieCBurgess <lucie.burgess@icloud.com>
2016-04-09 15:58:09 +0100

    First commit of coursework 4 All files as per Keith Mannock's repo, apart from interface ROOT.java (/src/gui/ROOT.java) in which the file path has been updated to read from my Eclipse workspace. This will need updating to the appropriate file location for the resource files (in folder /res) for users wishing to run the code.

---
+++ src/gui/ROOT.java
@@ -0,0 +1,6 @@
+package gui;
+
+interface ROOT {
+    //final static String ROOT = "/Users/keith/Courses/PiJ/repo/cw-temple/res/";
+	final static String ROOT = "/Users/lucieburgess/Documents/Birkbeck/Eclipse_workspace/Coursework4_Temple/res/";
+}
