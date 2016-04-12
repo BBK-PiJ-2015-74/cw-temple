390650a38c31f51ca321ba2192a9697fb996db8d
LucieCBurgess <lucie.burgess@icloud.com>
2016-04-12 21:12:25 +0100

    Tidied up comments, javadoc and explanations. Left in system.out.println statements which make code output easier to work through, although need to delete those before submission Code needs optimisation

--- src/exploring/TempleExplorer.java
+++ src/exploring/TempleExplorer.java
@@ -12,9 +12,8 @@ import game.NodeStatus;
 /**
  * @author BBK-PiJ-2015-74 lburge01
  * @see student/Explorer.java
+ * A class to explore the Temple of Gloom and claim the Orb
  */
-
-// need to be consistent between Tile and Node
 
 public class TempleExplorer {
 	
@@ -23,7 +22,6 @@ public class TempleExplorer {
 	private static final int MAX_DISTANCE = 1000000;
 	private Set<Long> tilesVisited;
 	private Stack<Long> pathStack;
-	private Boolean blindAlleyFound;
 
 	public TempleExplorer(ExplorationState state) {
 		this.currentState = state;
@@ -35,72 +33,64 @@ public class TempleExplorer {
 	 * A method to claim the Orb.
 	 * This method does the following:
 	 * Finds the neighbours of the current ExplorationState (neighbours)
-	 * Adds the id of the tile visited to a HashSet;
+	 * Adds the id of the tiles visited to a HashSet;
+	 * Finds the neighbour in the Collection<NodeStatus> neighbours which have not yet been visited;
+	 * Calculates a distance to target for the neighbour tile and compares this to a previously recorded distance;
+	 * Moves to the neighbour tile with a distance to the Orb less than the previously recorded distance, based on the grid
 	 * Records the path taken by adding the ids of the tiles visited to a stack;
-	 * Finds the neighbour in the Collection<NodeStatus> neighbour which is closest to the Orb based on the grid;
-	 * Checks that the nearest node is closer than any node previously found
-	 * Provides the id of the node, and moves to it
 	 * If a blind alley is found, retraces steps to a tile at which adjacent there is a tile that has not been visited
 	 */
 	public void claimTheOrb() {
 		
 		pathlength = 0;
 		long firstTileVisited = currentState.getCurrentLocation();
-		blindAlleyFound = false;
 		System.out.println("The first tile visited was " + firstTileVisited);
 		recordPath();
 		
-		while (currentState.getDistanceToTarget() != 0) { //deleted && blindAlleyFound == false
+		while (currentState.getDistanceToTarget() != 0) {
 			
-			System.out.println("Back to beginning of while loop");
+			//System.out.println("Back to beginning of while loop");
 			Collection<NodeStatus> neighbours = currentState.getNeighbours();
 			tilesVisited.add(currentState.getCurrentLocation());
-			//recordPath();
 			int distance = MAX_DISTANCE;
 			long nextTile = -1L;
 			
-			NodeStatus tileNotVisitedClosestToOrb = getNearestNeighbour(neighbours, tilesVisited);
-			System.out.println("Entering if statement");
-			//Finds the tile which has not been visited with the shortest path to the Orb
-			// returns the NodeStatus of the tile, or else null
-			// if null, sets blindAlleyFound to true
+			NodeStatus neighbourTilesNotVisited = findNeighbourTilesNotVisited(neighbours, tilesVisited);
+			//System.out.println("Entering if statement");
+			//neighbourTilesNotVisited is null if all neighbouring tiles to the current location have been visited
 			
-			if (tileNotVisitedClosestToOrb != null && tileNotVisitedClosestToOrb.getDistanceToTarget() < distance) { 
-					distance = tileNotVisitedClosestToOrb.getDistanceToTarget();
-					nextTile = tileNotVisitedClosestToOrb.getId();
-					System.out.println("Moving to tile with id: " + nextTile); 
-					System.out.println("Moving from current position: " + currentState.getCurrentLocation());
-					currentState.moveTo(nextTile); //move to the adjacent tile which has not been visited
-					recordPath();
-					System.out.println("\t to new position: " + currentState.getCurrentLocation());
-					pathlength++;
-					System.out.println("Number of steps taken is " + pathlength);
-			//} else if (tileNotVisitedClosestToOrb != null){
-			//	retraceStep();
+			if (neighbourTilesNotVisited != null && neighbourTilesNotVisited.getDistanceToTarget() < distance) { 
+			// finds the neighbourTileNotVisited which is closest to the Orb based on the grid
+			// works on grid not graph: could be optimised
+				distance = neighbourTilesNotVisited.getDistanceToTarget();
+				nextTile = neighbourTilesNotVisited.getId();
+				System.out.println("Moving to tile with id: " + nextTile); 
+				System.out.println("Moving from current position: " + currentState.getCurrentLocation());
+				currentState.moveTo(nextTile); 
+				recordPath();
+				System.out.println("\t to new position: " + currentState.getCurrentLocation());
+				pathlength++;
+				System.out.println("Number of steps taken is " + pathlength);
 			} else {
 				retraceStep();
-			}
-			
-			
-		}// end of while loop
-		
-	} // end of method claimTheOrb
+			}	
+		}	
+	}
 	
 	/**
-	 * A private method to return the NodeStatus of the nearest non-visited neighbour
-	 * @param neighbours
-	 * @param tileVisited
-	 * @return NodeStatus of the tile which is in the set of neighbours, is nearest to the Orb, and has not been visited before
+	 * A private method to return the NodeStatus the current tile, and find the tile in the set of neighbours which has not been visited closest to the Orb
+	 * @param neighbours, the neighbouring tiles to the current location
+	 * @param tileVisited, the set of tiles which have been visited by Lara so far
+	 * @return NodeStatus of the tile which is in the set of neighbours and has not been visited before
 	 */
-	private NodeStatus getNearestNeighbour(Collection<NodeStatus> neighbours, Set<Long> tileVisited) {
-		System.out.println("Calling getNearestNeighbour");
+	private NodeStatus findNeighbourTilesNotVisited(Collection<NodeStatus> neighbours, Set<Long> tileVisited) {
+		//System.out.println("calling findNeighbourTilesNotVisited sorted by NodeStatus");
 		NodeStatus tileNotVisited = neighbours.stream()
             .sorted(NodeStatus::compareTo) 
             .filter(n -> !tileVisited.contains(n.getId()))
             .findFirst().orElse(null);
-		if (tileNotVisited == null) { // tile has already been visited
+		if (tileNotVisited == null) { // all neighbouring tiles have already been visited
 			System.out.println("Blind alley has been found");
-			blindAlleyFound = true;
 		}
 		return tileNotVisited;
 	}
@@ -112,7 +102,7 @@ public class TempleExplorer {
 	 */
 	private Stack<Long> recordPath() {
 		pathStack.push(currentState.getCurrentLocation());
-		System.out.println("Recording path: location "+ pathStack.peek());
+		//System.out.println("Recording path: location "+ pathStack.peek());
 		return pathStack;
 	}
 	
@@ -120,13 +110,11 @@ public class TempleExplorer {
 	 * A private method to retrace the steps travelled by Lara when she has hit a blind alley.
 	 */
 	private void retraceStep() {
-		System.out.println("Calling retraceStep() method");
-		System.out.println("The current location is " + currentState.getCurrentLocation());
+		//System.out.println("Calling retraceStep() method");
 		pathStack.pop();
 		System.out.println("The current location is " + currentState.getCurrentLocation());
 		System.out.println("The next location is " + pathStack.peek());
 		currentState.moveTo(pathStack.peek());	
-		blindAlleyFound = false;
 		pathlength++;
 	}
 
